6fe07bb23be11d98850cd85ba2228216c34c0dd7
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2018-02-06 22:04:25 +0900

    [experimental] predicte model

--- C₂/Container+MNIST.swift
+++ C₂/Container+MNIST.swift
@@ -18,6 +18,9 @@ extension Container {
 		public var family: String {
 			return String(describing: self)
 		}
+		public var predicate: NSPredicate {
+			return NSPredicate(format: "domain = %@ and family = %@", argumentArray: [String(describing: type(of: self)), String(describing: self)])
+		}
 	}
 	public enum MNISTError: Error {
 		case format
@@ -27,21 +30,31 @@ private extension Container {
 	private func cache(mnist: MNIST) throws -> (image: URL, label: URL) {
 		let baseURL: URL = cache.appendingPathComponent(MNIST.domain, isDirectory: true).appendingPathComponent(mnist.family, isDirectory: true)
 		try FileManager.default.createDirectory(at: baseURL, withIntermediateDirectories: true, attributes: nil)
-		return(image: baseURL.appendingPathComponent(imageKey),
-			   label: baseURL.appendingPathComponent(labelKey))
+		return(image: baseURL.appendingPathComponent(imageKey), label: baseURL.appendingPathComponent(labelKey))
 	}
 }
 private extension Container {
-	private func endian(value: UInt32) -> Int {
-		return Int(UInt32(bigEndian: value))
-	}
-	private func update(mnist: MNIST, context: NSManagedObjectContext, image: URL, label: URL) throws {
+	private func update(mnist: MNIST, context: NSManagedObjectContext) throws {
 		try context.fetch(series: mnist).forEach {
 			context.delete($0)
 			try context.save()
 		}
-		let labelHandle: FileHandle = try FileHandle(forReadingFrom: label)
-		let imageHandle: FileHandle = try FileHandle(forReadingFrom: image)
+		let(imageCache, labelCache): (URL, URL) = try cache(mnist: mnist)
+		let fileManager: FileManager = .default
+		let imageURL: URL = fileManager.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+		let labelURL: URL = fileManager.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+		if !fileManager.fileExists(atPath: imageURL.path) {
+			fileManager.createFile(atPath: imageURL.path, contents: nil, attributes: nil)
+		}
+		if !fileManager.fileExists(atPath: labelURL.path) {
+			fileManager.createFile(atPath: labelURL.path, contents: nil, attributes: nil)
+		}
+		let labelHandle: FileHandle = try FileHandle(forUpdating: labelURL)
+		let imageHandle: FileHandle = try FileHandle(forUpdating: imageURL)
+		try Data(contentsOf: labelCache, options: .mappedIfSafe).gunzip(to: labelHandle)
+		try Data(contentsOf: imageCache, options: .mappedIfSafe).gunzip(to: imageHandle)
+		labelHandle.seek(toFileOffset: 0)
+		imageHandle.seek(toFileOffset: 0)
 		guard
 			let labelheader: [UInt32] = labelHandle.readArray(count: 2),
 			let imageheader: [UInt32] = imageHandle.readArray(count: 4) else {
@@ -77,22 +90,20 @@ private extension Container {
 			image.data = pixel
 			try context.save()
 		}
+		labelHandle.closeFile()
+		imageHandle.closeFile()
+		try fileManager.removeItem(at: labelURL)
+		try fileManager.removeItem(at: imageURL)
 		notification?.success(build: mnist)
 	}
-	private func update(mnist: MNIST, image: URL, label: URL) {
+	private func update(mnist: MNIST) throws {
 		func dispatch(context: NSManagedObjectContext) {
 			do {
-				let fileManager: FileManager = .default
-				try update(mnist: mnist, context: context, image: image, label: label)
-				try fileManager.removeItem(at: image)
-				try fileManager.removeItem(at: label)
+				try update(mnist: mnist, context: context)
 			} catch {
-				notification?.failure(error: error)
+				failure(error: error)
 			}
 		}
-		performBackgroundTask(dispatch)
-	}
-	private func update(mnist: MNIST) throws {
 		func dispatch() {
 			do {
 				let (image, label): (URL, URL) = try cache(mnist: mnist)
@@ -100,13 +111,9 @@ private extension Container {
 				guard fileManager.fileExists(atPath: image.path), fileManager.fileExists(atPath: label.path) else {
 					return
 				}
-				let tmpImage: URL = fileManager.temporaryDirectory.appendingPathComponent(UUID().uuidString)
-				let tmpLabel: URL = fileManager.temporaryDirectory.appendingPathComponent(UUID().uuidString)
-				try fileManager.moveItem(at: image, to: tmpImage)
-				try fileManager.moveItem(at: label, to: tmpLabel)
-				update(mnist: mnist, image: tmpImage, label: tmpLabel)
+				performBackgroundTask(dispatch)
 			} catch {
-				notification?.failure(error: error)
+				failure(error: error)
 			}
 		}
 		viewContext.perform(dispatch)//execlusive execution
@@ -114,19 +121,19 @@ private extension Container {
 }
 private extension Container {
 	@objc private func mnistrain(image mnist: URL) throws {
-		try Data(contentsOf: mnist, options: .mappedIfSafe).gunzip(to: cache(mnist: .train).image)
+		try FileManager.default.moveItem(at: mnist, to: cache(mnist: .train).image)
 		try update(mnist: .train)
 	}
 	@objc private func mnistrain(label mnist: URL) throws {
-		try Data(contentsOf: mnist, options: .mappedIfSafe).gunzip(to: cache(mnist: .train).label)
+		try FileManager.default.moveItem(at: mnist, to: cache(mnist: .train).label)
 		try update(mnist: .train)
 	}
 	@objc private func mnist10k(image mnist: URL) throws {
-		try Data(contentsOf: mnist, options: .mappedIfSafe).gunzip(to: cache(mnist: .t10k).image)
+		try FileManager.default.moveItem(at: mnist, to: cache(mnist: .t10k).image)
 		try update(mnist: .t10k)
 	}
 	@objc private func mnist10k(label mnist: URL) throws {
-		try Data(contentsOf: mnist, options: .mappedIfSafe).gunzip(to: cache(mnist: .t10k).label)
+		try FileManager.default.moveItem(at: mnist, to: cache(mnist: .t10k).label)
 		try update(mnist: .t10k)
 	}
 	private func selector(mnist: MNIST, key: String) throws -> String {
@@ -148,21 +155,24 @@ extension Container {
 	public func build(mnist: MNIST) throws {
 		let fileManager: FileManager = .default
 		let (imageURL, labelURL): (URL, URL) = try cache(mnist: mnist)
-		guard let dictionary: [String: URL] = global[MNIST.domain]?[mnist.family] else {
+		guard let dictionary: [String: String] = try plist(series: mnist)[mnist.family]as?[String: String] else {
 			throw ErrorCases.dictionary
 		}
 		let stable: Bool = try [(imageURL, imageKey), (labelURL, labelKey)].reduce(true) {
 			guard !fileManager.fileExists(atPath: $1.0.path) else {
 				return $0
 			}
-			guard let global: URL = dictionary[$1.1] else {
-				throw ErrorCases.url
+			guard
+				let string: String = dictionary[$1.1],
+				let url: URL = URL(string: string) else {
+					throw ErrorCases.url
 			}
-			let downloadTask: URLSessionDownloadTask = urlsession.downloadTask(with: global)
+			let downloadTask: URLSessionDownloadTask = urlsession.downloadTask(with: url)
 			downloadTask.taskDescription = try selector(mnist: mnist, key: $1.1)
 			downloadTask.resume()
 			return false
 		}
+		print(stable)
 		if stable {
 			try update(mnist: mnist)
 		}
