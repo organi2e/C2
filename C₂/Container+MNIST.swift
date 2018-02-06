07e9eaba2a12f01eef3dcfd58df0c40df8553533
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2018-02-06 22:07:28 +0900

    redundancy model

--- C₂/Container+MNIST.swift
+++ C₂/Container+MNIST.swift
@@ -49,15 +49,17 @@ private extension Container {
 		if !fileManager.fileExists(atPath: labelURL.path) {
 			fileManager.createFile(atPath: labelURL.path, contents: nil, attributes: nil)
 		}
-		let labelHandle: FileHandle = try FileHandle(forUpdating: labelURL)
-		let imageHandle: FileHandle = try FileHandle(forUpdating: imageURL)
-		try Data(contentsOf: labelCache, options: .mappedIfSafe).gunzip(to: labelHandle)
-		try Data(contentsOf: imageCache, options: .mappedIfSafe).gunzip(to: imageHandle)
-		labelHandle.seek(toFileOffset: 0)
-		imageHandle.seek(toFileOffset: 0)
+		let wlabelHandle: FileHandle = try FileHandle(forWritingTo: labelURL)
+		let wimageHandle: FileHandle = try FileHandle(forWritingTo: imageURL)
+		try Data(contentsOf: labelCache, options: .mappedIfSafe).gunzip(to: wlabelHandle)
+		try Data(contentsOf: imageCache, options: .mappedIfSafe).gunzip(to: wimageHandle)
+		wlabelHandle.closeFile()
+		wimageHandle.closeFile()
+		let rlabelHandle: FileHandle = try FileHandle(forReadingFrom: labelURL)
+		let rimageHandle: FileHandle = try FileHandle(forReadingFrom: imageURL)
 		guard
-			let labelheader: [UInt32] = labelHandle.readArray(count: 2),
-			let imageheader: [UInt32] = imageHandle.readArray(count: 4) else {
+			let labelheader: [UInt32] = rlabelHandle.readArray(count: 2),
+			let imageheader: [UInt32] = rimageHandle.readArray(count: 4) else {
 				throw MNISTError.format
 		}
 		let labelheads: [Int] = labelheader.map { Int(UInt32(bigEndian: $0)) }
@@ -73,8 +75,8 @@ private extension Container {
 		let bytes: Int = rows * cols
 		try Array(repeating: (), count: count).forEach {
 			guard
-				let pixel: Data = imageHandle.readData(count: bytes),
-				let label: UInt8 = labelHandle.readElement() else {
+				let label: UInt8 = rlabelHandle.readElement(),
+				let pixel: Data = rimageHandle.readData(count: bytes) else {
 					throw MNISTError.format
 			}
 			let image: Image = Image(in: context)
@@ -90,8 +92,8 @@ private extension Container {
 			image.data = pixel
 			try context.save()
 		}
-		labelHandle.closeFile()
-		imageHandle.closeFile()
+		rlabelHandle.closeFile()
+		rimageHandle.closeFile()
 		try fileManager.removeItem(at: labelURL)
 		try fileManager.removeItem(at: imageURL)
 		notification?.success(build: mnist)
