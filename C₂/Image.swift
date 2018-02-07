121cc7aed1cd291a6bc4bf4260ee14ae2fee447c
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2018-02-07 22:11:39 +0900

    array accessor

--- C₂/Image.swift
+++ C₂/Image.swift
@@ -17,6 +17,36 @@ extension Image {
 	@NSManaged var format: CIFormat
 	@NSManaged var data: Data
 }
+extension Image {
+	public var la: la_object_t {
+		return la_splat_from_float(1, la_attribute_t(LA_ATTRIBUTE_ENABLE_LOGGING))
+	}
+	public var array: [Float] {
+		switch format {
+		case kCIFormatAf, kCIFormatRf, kCIFormatRGBAf:
+			return data.withUnsafeBytes {
+				Array(UnsafeBufferPointer(start: $0, count: data.count / MemoryLayout<Float>.stride))
+			}
+		case kCIFormatA8, kCIFormatR8, kCIFormatARGB8, kCIFormatBGRA8:
+			let result: [Float] = [Float](repeating: 0, count: data.count / MemoryLayout<UInt8>.stride)
+			data.withUnsafeBytes {
+				vDSP_vfltu8($0, 1, UnsafeMutablePointer(mutating: result), 1, vDSP_Length(result.count))
+			}
+			cblas_sscal(Int32(result.count), 1/256.0, UnsafeMutablePointer(mutating: result), 1)
+			return result
+		case kCIFormatA16, kCIFormatR16, kCIFormatRGBA16:
+			let result: [Float] = [Float](repeating: 0, count: data.count / MemoryLayout<UInt16>.stride)
+			data.withUnsafeBytes {
+				vDSP_vfltu16($0, 1, UnsafeMutablePointer(mutating: result), 1, vDSP_Length(result.count))
+			}
+			cblas_sscal(Int32(result.count), 1/65536.0, UnsafeMutablePointer(mutating: result), 1)
+			return result
+		default:
+			assertionFailure("CIImage format: \(format) has been not implemented")
+			return Array<Float>()
+		}
+	}
+}
 public extension Image {
 	var size: NSSize {
 		return NSMakeSize(CGFloat(width), CGFloat(height))
@@ -27,6 +57,6 @@ public extension Image {
 		}
 	}
 	var ciImage: CIImage {
-		return CIImage(bitmapData: data, bytesPerRow: Int(rowBytes), size: size, format: kCIFormatA8, colorSpace: nil)
+		return CIImage(bitmapData: data, bytesPerRow: Int(rowBytes), size: size, format: format, colorSpace: nil)
 	}
 }
