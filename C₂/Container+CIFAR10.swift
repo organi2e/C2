37005d0ebfc3c325e4c1194bb26897f9c2be0e03
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2018-02-16 11:22:48 +0900

    arrange mint parsing

--- C₂/Container+CIFAR10.swift
+++ C₂/Container+CIFAR10.swift
@@ -29,11 +29,55 @@ public extension Container {
 	}
 }
 private extension Container {
-	private var cachecifar10: URL {
-		return cache.appendingPathComponent(CIFAR10.domain)
+	private func cache(cifar10: Void) throws -> URL {
+		let baseURL: URL = cache.appendingPathComponent(CIFAR10.domain, isDirectory: true)
+		try FileManager.default.createDirectory(at: baseURL, withIntermediateDirectories: true, attributes: nil)
+		return baseURL.appendingPathComponent("tgz")
+	}
+}
+private extension Data {
+	private func planner2RGBX(with: UnsafePointer<UInt8>) -> Data {
+		return Data()
+	}
+	var planner2rgb: Data {
+		return withUnsafeBytes(planner2RGBX)
 	}
 }
 private extension Container {
+	private func rebuild(cifar10: CIFAR10, rows: Int, cols: Int, data: Data, context: NSManagedObjectContext) throws {
+		try context.index(series: cifar10).forEach(context.delete)
+		let data: [(UInt8, Data)] = data.chunk(width: rows * cols * 3 + 1).map {
+			($0.toValue(), $0[1..<$0.count])
+		}
+		let height: vImagePixelCount = vImagePixelCount(rows)
+		let width: vImagePixelCount = vImagePixelCount(cols)
+		let rowBytes: Int = 4 * cols
+		let images: [UInt8: Set<Image>] = data.reduce([UInt8: Set<Image>]()) {
+			let head: UInt8 = $1.0
+			let tail: Data = $1.1
+			let image: Image = Image(in: context)
+			image.width = UInt16(cols)
+			image.height = UInt16(rows)
+			image.rowBytes = UInt32(rowBytes)
+			image.format = kCIFormatRGBA8
+			image.data = Data(count: rowBytes * Int(height))
+			image.data.withUnsafeMutableBytes { (data: UnsafeMutablePointer<UInt8>) in
+				let result: vImage_Error = tail.withUnsafeBytes {
+					vImageConvert_Planar8ToBGRX8888([vImage_Buffer(data: UnsafeMutablePointer<UInt8>(mutating: $0).advanced(by: 2*Int(height*width)), height: height, width: width, rowBytes: rowBytes)],
+													[vImage_Buffer(data: UnsafeMutablePointer<UInt8>(mutating: $0).advanced(by: 1*Int(height*width)), height: height, width: width, rowBytes: rowBytes)],
+													[vImage_Buffer(data: UnsafeMutablePointer<UInt8>(mutating: $0).advanced(by: 0*Int(height*width)), height: height, width: width, rowBytes: rowBytes)],
+													255,
+													[vImage_Buffer(data: data, height: height, width: width, rowBytes: rowBytes)],
+													0)
+				}
+				assert(result == kvImageNoError)
+			}
+			return $0.merging([head: Set<Image>(arrayLiteral: image)]) {
+				$0.union($1)
+			}
+		}
+		
+	}
 	private func buildcifar10(context: NSManagedObjectContext, rows: Int, cols: Int, family: String, data: Data) throws {
 		try data.chunk(width: rows * cols * 3 + 1).forEach {
 			let head: UInt8 = $0.toValue()
@@ -67,7 +111,7 @@ private extension Container {
 			}
 		}
 	}
-	private func buildcifar10(context: NSManagedObjectContext) throws {
+	private func rebuild(cifar10: Void, context: NSManagedObjectContext) throws {
 		let dictionary: [String: Any] = try plist(series: CIFAR10.test)
 		guard
 			let rows: Int = dictionary[rowsKey]as?Int,
@@ -83,22 +127,22 @@ private extension Container {
 				throw ErrorCases.dictionary
 		}
 		var labels: [UInt8: String] = [:]
-		try Untar(supplier: Gunzip(url: cachecifar10, options: .mappedRead, maximum: rows * cols * MemoryLayout<UInt8>.stride * 4)).scan {
+		try Untar(supplier: Gunzip(url: cache(cifar10: ()), options: .mappedRead, maximum: rows * cols * MemoryLayout<UInt8>.stride * 4)).scan {
 			switch $0 {
 			case batch1:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch1.family, data: $1)
+				try rebuild(cifar10: .batch1, rows: rows, cols: cols, data: $1, context: context)
 			case batch2:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch2.family, data: $1)
+				try rebuild(cifar10: .batch2, rows: rows, cols: cols, data: $1, context: context)
 			case batch3:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch3.family, data: $1)
+				try rebuild(cifar10: .batch3, rows: rows, cols: cols, data: $1, context: context)
 			case batch4:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch4.family, data: $1)
+				try rebuild(cifar10: .batch4, rows: rows, cols: cols, data: $1, context: context)
 			case batch5:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch5.family, data: $1)
+				try rebuild(cifar10: .batch5, rows: rows, cols: cols, data: $1, context: context)
 			case batch6:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.batch6.family, data: $1)
+				try rebuild(cifar10: .batch6, rows: rows, cols: cols, data: $1, context: context)
 			case test:
-				try buildcifar10(context: context, rows: rows, cols: cols, family: CIFAR10.test.family, data: $1)
+				try rebuild(cifar10: .test, rows: rows, cols: cols, data: $1, context: context)
 			case meta:
 				guard let text: String = String(data: $1, encoding: .utf8) else {
 					throw "meta file is wrong"
@@ -112,38 +156,52 @@ private extension Container {
 		}
 		print(labels)
 	}
-	func buildcifar10() throws {
+	func rebuild(cifar10: Void) throws {
 		func dispatch(context: NSManagedObjectContext) {
 			do {
-				try buildcifar10(context: context)
+				try rebuild(cifar10: (), context: context)
 			} catch {
-				
+				failure(error: error)
 			}
 		}
-		performBackgroundTask(dispatch)
+		func dispatch() {
+			do {
+				let cacheURL: URL = try cache(cifar10: ())
+				let fileManager: FileManager = .default
+				guard fileManager.fileExists(atPath: cacheURL.path) else {
+					return
+				}
+				performBackgroundTask(dispatch)
+			} catch {
+				failure(error: error)
+			}
+		}
+		viewContext.perform(dispatch)//execlusive execution
 	}
 }
 private extension Container {
 	@objc private func cifar10(location: URL) throws {
-		try FileManager.default.moveItem(at: location, to: cachecifar10)
-		try buildcifar10()
+		try FileManager.default.moveItem(at: location, to: cache(cifar10: ()))
+		try rebuild(cifar10: ())
 	}
-	private var selectorcifar10: String {
+	private func selector(cifar10: Void) -> String {
 		return #selector(cifar10(location:)).description
 	}
 }
 public extension Container {
 	public func build(cifar10: CIFAR10) throws {
+		let fileManager: FileManager = .default
+		let cacheURL: URL = try cache(cifar10: ())
 		guard
-			let string: String = try plist(series: cifar10)[urlKey]as?String,
-			let url: URL = URL(string: string) else {
-				throw ErrorCases.url
+			let urlstring = try plist(series: cifar10)[urlKey]as?String,
+			let url: URL = URL(string: urlstring) else {
+				throw ErrorCases.dictionary
 		}
-		if FileManager.default.fileExists(atPath: cachecifar10.path) {
-			try buildcifar10()
+		if fileManager.fileExists(atPath: cacheURL.path) {
+			try rebuild(cifar10: ())
 		} else if !isDownloading(url: url) {
 			let downloadTask: URLSessionDownloadTask = download(url: url)
-			downloadTask.taskDescription = selectorcifar10
+			downloadTask.taskDescription = selector(cifar10: ())
 			downloadTask.resume()
 		}
 	}
