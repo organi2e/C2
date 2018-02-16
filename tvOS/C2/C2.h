a98f0b26365f8a595b68477809c226b2fc06daca
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2018-02-16 18:35:18 +0900

    several platforms

---
+++ tvOS/C2/C2.h
@@ -0,0 +1,18 @@
+//
+//  C2.h
+//  C2
+//
+//  Created by Kota Nakano on 2/16/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for C2.
+FOUNDATION_EXPORT double C2VersionNumber;
+
+//! Project version string for C2.
+FOUNDATION_EXPORT const unsigned char C2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <C2/PublicHeader.h>
+
+
